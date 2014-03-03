42d5186712637d58c41735e9b81574e1a42e9637
androidzhaoxiaogang <18008021570@189.cn>
2014-03-03 11:25:22 +0800

    init version;

---
+++ src/xg/elasticlistview/ElasticListView.java
@@ -0,0 +1,24 @@
+package xg.elasticlistview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class ElasticListView extends ListView {
+
+	public ElasticListView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ElasticListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ElasticListView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
